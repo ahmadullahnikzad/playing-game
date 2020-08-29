f41cbf9f2950fd60a889b5cb8da0c941d4e85053
Ahmadullah Nikzad <ahmadullahnikzad1919@gmail.com>
2020-08-29 13:50:21 +0000

    i made a function

--- script.js
+++ script.js
@@ -3,10 +3,20 @@ Rock, Scissors ->The Rock choice wins, as Rock can blunt Scissors.
 Paper, Rock ->The Paper choice wins, as Paper can cover Rock.*/
 let computerChoice=['rock','paper','scissors'];
 let myChoice=prompt('enter your choice');
-let status;
+// let status;
+// if(myChoice==computerChoice[Math.floor(Math.random()*computerChoice.length)]){
+//   status=true;
+// }else{
+//   status=false;
+// }
+// console.log(status);
+function gamePlay(computerChoice,myChoice){
+  let status;
 if(myChoice==computerChoice[Math.floor(Math.random()*computerChoice.length)]){
   status=true;
 }else{
   status=false;
 }
 console.log(status);
+}
+gamePlay(computerChoice,myChoice);
