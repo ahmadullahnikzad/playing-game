c2f19e47a0a4f569259fb37eadfe20d013a2d57f
Ahmadullah Nikzad <ahmadullahnikzad1919@gmail.com>
2020-08-29 13:46:29 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+/*Scissors, Paper -> The Scissors choice wins, as Scissors can cut Paper.
+Rock, Scissors ->The Rock choice wins, as Rock can blunt Scissors.
+Paper, Rock ->The Paper choice wins, as Paper can cover Rock.*/
+let computerChoice=['rock','paper','scissors'];
+let myChoice=prompt('enter your choice');
+let status;
+if(myChoice==computerChoice[Math.floor(Math.random()*computerChoice.length)]){
+  status=true;
+}else{
+  status=false;
+}
+console.log(status);
